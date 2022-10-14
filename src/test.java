4a62b5192a195aab56cceff86bc71f9b329bf1b3
Stanford <ok921729@gmail.com>
2022-10-14 13:57:55 +0800

    新增建立txt以及寫入txt function

--- src/test.java
+++ src/test.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -10,7 +7,12 @@ import java.util.List;
 
 public class test {
 
+    // 建立文字檔路徑
+    private static String path = "D:/";
+    private static String filenameTemp;
+
     public static void main(String[] args) throws IOException, ParseException {
+
 
         List<String> calculate = new ArrayList<>();
         double ThirtyThreeOverTimeHr = 0.0;
@@ -29,7 +31,6 @@ public class test {
         buffReader.close();
 
         for (int i=3; i< calculate.size();i++){
-
 
             String[] data = calculate.get(i).split(",");
 //            System.out.println(calculate.get(i));
@@ -55,6 +56,15 @@ public class test {
 
     }
 
+    /**
+     * 計算加班時數
+     *
+     * @param onboardTime
+     *
+     * @param offboardTime
+     *
+     * @throws ParseException
+     */
     private static long CalculateOverTime(String onboardTime, String offboardTime) throws ParseException {
 
         String standardDate="2022/10/14 ";
@@ -87,12 +97,94 @@ public class test {
 
         Long timeP=ut2-ut1;//毫秒差
 
-//        Long sec=timeP/1000;//秒差
-
         Long overTime=timeP/(1000*60);//分差
 
         return overTime;
     }
 
+    /**
+     * 建立檔案
+     *
+     * @throws IOException
+     */
+    public static boolean creatTxtFile(String name) throws IOException {
+        boolean flag = false;
+        filenameTemp = path + name + ".txt";
+        File filename = new File(filenameTemp);
+        if (!filename.exists()) {
+            filename.createNewFile();
+            flag = true;
+        }
+        return flag;
+    }
+
+    /**
+     * 寫檔案
+     *
+     * @param newStr
+     *            新內容
+     * @throws IOException
+     */
+    public static boolean writeTxtFile(String newStr) throws IOException {
+        // 先讀取原有檔案內容，然後進行寫入操作
+        boolean flag = false;
+        String filein = newStr + "\r\n";
+        String temp = "";
+
+        FileInputStream fis = null;
+        InputStreamReader isr = null;
+        BufferedReader br = null;
+
+        FileOutputStream fos = null;
+        PrintWriter pw = null;
+
+        try {
+            // 檔案路徑
+            File file = new File(filenameTemp);
+            // 將檔案讀入輸入流
+            fis = new FileInputStream(file);
+            isr = new InputStreamReader(fis);
+            br = new BufferedReader(isr);
+            StringBuffer buf = new StringBuffer();
+
+            // 儲存該檔案原有的內容
+            for (int j = 1; (temp = br.readLine()) != null; j++) {
+                buf = buf.append(temp);
+                // System.getProperty("line.separator")
+                // 行與行之間的分隔符 相當於“\n”
+                buf = buf.append(System.getProperty("line.separator"));
+            }
+            buf.append(filein);
+
+            fos = new FileOutputStream(file);
+            pw = new PrintWriter(fos);
+            pw.write(buf.toString().toCharArray());
+            pw.flush();
+            flag = true;
+        } catch (IOException e1) {
+            // TODO 自動生成 catch 塊
+            throw e1;
+        } finally {
+            if (pw != null) {
+                pw.close();
+            }
+            if (fos != null) {
+                fos.close();
+            }
+            if (br != null) {
+                br.close();
+            }
+            if (isr != null) {
+                isr.close();
+            }
+            if (fis != null) {
+                fis.close();
+            }
+        }
+        return flag;
+    }
 
 }
+
+
+
